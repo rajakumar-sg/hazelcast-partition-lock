8ed84e9e3ad3205bacde8d152c59f46c619867a2
raj <torajakumar@gmail.com>
2018-10-22 20:51:30 +0800

    initial

---
+++ src/main/java/com/haztest/App.java
@@ -0,0 +1,13 @@
+package com.haztest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
